89727bf18575be98218e9ed3906b21c9e412adb8
klkirstyleeskl <k.lees@btinternet.com>
2019-02-08 12:32:04 +0000

    adds UI test

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -19,9 +19,44 @@ describe('calculator functionality', function() {
   it('number buttons update the display', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click()
-    element(by.css('#number3')).click()
-    ;
+    element(by.css('#number3')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('23')
+  })
+
+  it('arithmetical functions update the display_Add', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click()
+    element(by.css('#operator_add')).click()
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  })
+
+  it('arithmetical functions update the display_Subtract', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click()
+    element(by.css('#operator_subtract')).click()
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('arithmetical functions update the display_Divide', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number6')).click()
+    element(by.css('#operator_divide')).click()
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('arithmetical functions update the display_Multiply', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click()
+    element(by.css('#operator_multiply')).click()
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('12')
   })
 
 
